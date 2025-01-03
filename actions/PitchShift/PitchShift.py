cbf0aeb5990bc4ac255da01d0315e857079d4679
Douglas Metcalfe <douglas.metcalfe14@bathspa.ac.uk>
2025-01-03 19:24:34 +0000

    fixed constructor

--- actions/PitchShift/PitchShift.py
+++ actions/PitchShift/PitchShift.py
@@ -8,8 +8,10 @@ from src.backend.PluginManager.PluginBase import PluginBase
 import os
 
 class PitchShift(ActionBase):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, action_id: str, action_name: str,
+                 deck_controller: DeckController, page: Page, coords: str, plugin_base: PluginBase):
+        super().__init__(action_id=action_id, action_name=action_name,
+            deck_controller=deck_controller, page=page, coords=coords, plugin_base=plugin_base)
 
         backend_path = os.path.join(self.plugin_base.PATH, "actions", "PitchShift", "backend", "backend.py") 
         self.launch_backend(backend_path = backend_path, open_in_terminal = True) 
