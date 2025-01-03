a047b9634078906c4d2dd4656ef0392d9796cb1d
Douglas Metcalfe <douglas.metcalfe14@bathspa.ac.uk>
2025-01-03 20:22:01 +0000

    moved backend

---
+++ actions/PitchShift/backend/backend.py
@@ -0,0 +1,25 @@
+from streamcontroller_plugin_tools import BackendBase 
+from pythonosc import udp_client
+
+class Backend(BackendBase):
+    def __init__(self):
+        super().__init__()
+        
+        self.client = udp_client.SimpleUDPClient("127.0.0.1", 22752)
+        self.count: int = 0
+
+    def get_count(self) -> int:
+        return self.count
+
+    def on_long_press(self):
+        self.count -= 1
+        self.send_message()
+
+    def on_short_press(self):
+        self.count += 1
+        self.send_message()
+
+    def send_message(self):
+        self.client.send_message("/Carla/0/set_parameter_value", [2, float(self.count)])
+
+backend = Backend()
